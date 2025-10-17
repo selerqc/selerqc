89e1f065fcf19668db438a340105d85fab5319d6
selerqc <agustincharles34@gmail.com>
2025-10-18 05:53:42 +0800

    Refactor environment variable names for consistency in build.yml and today.py

--- today.py
+++ today.py
@@ -7,8 +7,8 @@ import time
 import hashlib
 
 
-HEADERS = {'authorization': 'token '+ os.environ['ACCESS_TOKEN']}
-USER_NAME = os.environ['METRICS_GIT_USER_NAME'] 
+HEADERS = {'authorization': 'token '+ os.environ['TOKEN']}
+USER_NAME = os.environ['USERNAME'] 
 QUERY_COUNT = {'user_getter': 0, 'follower_getter': 0, 'graph_repos_stars': 0, 'recursive_loc': 0, 'graph_commits': 0, 'loc_query': 0}
 
 
